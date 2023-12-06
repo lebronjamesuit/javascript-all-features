0f6565cf3798fb424bb4be87588da81e33abc618
lebronjamesuit <votrannhatminh1508@gmail.com>
2023-12-06 20:51:56 +0000

    DOM manipulative

---
+++ DOMcss/colors.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    //alert('DOMContentLoaded');
+
+    document.querySelector('#red').addEventListener('click', function () {
+        document.querySelector('#color').
+    });
+})
